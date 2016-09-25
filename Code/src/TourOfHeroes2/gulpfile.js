3c3903be9a565c061f37667fa4d3c966aed8e797
Brady Kelly <brady@bradykelly.net>
2016-09-25 13:51:36 +0200

    Just trying to get rid of extra src dir.

--- Code/src/TourOfHeroes2/gulpfile.js
+++ Code/src/TourOfHeroes2/gulpfile.js
@@ -12,6 +12,7 @@ var js = [
     "./node_modules/zone.js/dist/zone.js",
     "./node_modules/reflect-metadata/Reflect.js",
     "./node_modules/systemjs/dist/system.src.js",
+    "./app/*.js",
     "./systemjs.config.js"
 ];
 
