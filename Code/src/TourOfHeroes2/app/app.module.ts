fd7bbbd95036ff9b5509b22802c41b03491943ec
Brady Kelly <brady@bradykelly.net>
2016-09-25 12:06:46 +0200

    Nearly working, but typescript not compiling.

--- Code/src/TourOfHeroes2/app/app.module.ts
+++ Code/src/TourOfHeroes2/app/app.module.ts
@@ -1,6 +1,6 @@
 ﻿import { NgModule }      from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { AppComponent }   from './app.component';
+import { AppComponent }   from "./app.component";
 
 
 @NgModule({
