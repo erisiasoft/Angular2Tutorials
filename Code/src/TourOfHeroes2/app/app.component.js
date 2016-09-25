2321325c70026e320323cabbbb4fc6c640563975
Brady Kelly <brady@bradykelly.net>
2016-09-25 13:36:55 +0200

    Sorted out duplicate identifiers in TypeScript excluding files in Typings folder.

--- Code/src/TourOfHeroes2/app/app.component.js
+++ Code/src/TourOfHeroes2/app/app.component.js
@@ -8,18 +8,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require("@angular/core");
-var AppComponent = (function () {
-    function AppComponent() {
-    }
-    AppComponent = __decorate([
-        core_1.Component({
-            selector: "my-app",
-            template: "<h1>My First Angular App</h1>"
-        }), 
-        __metadata('design:paramtypes', [])
-    ], AppComponent);
-    return AppComponent;
-}());
+const _1 = require("@angular/core/");
+let AppComponent = class AppComponent {
+};
+AppComponent = __decorate([
+    _1.Component({
+        selector: "my-app",
+        template: "<h1>My First Angular App</h1>"
+    }), 
+    __metadata('design:paramtypes', [])
+], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
