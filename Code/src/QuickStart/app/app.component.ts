91816ae740545c44a4c78c954b94b7d021bdb40e
Brady Kelly <brady@bradykelly.net>
2016-09-29 13:41:54 +0200

    List of heroes working.

--- Code/src/QuickStart/app/app.component.ts
+++ Code/src/QuickStart/app/app.component.ts
@@ -5,10 +5,29 @@ export class Hero {
     name: string;
 }
 
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+
 @Component({
     selector: "my-app",
     template: `
       <h1>{{title}}</h1>
+      <h2>My Heroes</h2>
+      <ul class="heroes">
+          <li *ngFor="let hero of heroes">
+            <span class="badge">{{hero.id}}</span> {{hero.name}}
+          </li>
+      </ul>
       <h2>{{hero.name}} details!</h2>
       <div><label>Id: </label>{{hero.id}}</div>
       <div>
@@ -18,10 +37,13 @@ export class Hero {
     `
 })
 export class AppComponent {
+
     title = "Tour of Heroes";
     hero: Hero = {
         id: 1,
         name: "windstorm"
     };
+    heroes = HEROES;
 }
+
 
--- Code/src/QuickStart/wwwroot/app/app.component.js
+++ Code/src/QuickStart/wwwroot/app/app.component.js
@@ -12,6 +12,18 @@ const core_1 = require("@angular/core");
 class Hero {
 }
 exports.Hero = Hero;
+const HEROES = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
 let AppComponent = class AppComponent {
     constructor() {
         this.title = "Tour of Heroes";
@@ -19,6 +31,7 @@ let AppComponent = class AppComponent {
             id: 1,
             name: "windstorm"
         };
+        this.heroes = HEROES;
     }
 };
 AppComponent = __decorate([
@@ -26,6 +39,12 @@ AppComponent = __decorate([
         selector: "my-app",
         template: `
       <h1>{{title}}</h1>
+      <h2>My Heroes</h2>
+      <ul class="heroes">
+          <li *ngFor="let hero of heroes">
+            <span class="badge">{{hero.id}}</span> {{hero.name}}
+          </li>
+      </ul>
       <h2>{{hero.name}} details!</h2>
       <div><label>Id: </label>{{hero.id}}</div>
       <div>
